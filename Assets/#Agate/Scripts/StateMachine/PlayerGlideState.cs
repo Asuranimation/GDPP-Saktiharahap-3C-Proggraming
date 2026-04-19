2a92789b42874b4665a6167f8e2e9c7bc4a2d946
SunnySaktiGit <sakti.harahap@darkwagon.com>
2026-04-19 20:01:25 +0700

    feat: simplify glide to slow fall with animation only

--- Assets/#Agate/Scripts/StateMachine/PlayerGlideState.cs
+++ Assets/#Agate/Scripts/StateMachine/PlayerGlideState.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerGlideState : PlayerBaseState, IRootState
 {
     public PlayerGlideState(PlayerStateMachine currentContext, PlayerStateFactory playerStateFactory)
-    : base(currentContext, playerStateFactory)
+        : base(currentContext, playerStateFactory)
     {
         IsRootState = true;
     }
@@ -11,29 +11,30 @@ public class PlayerGlideState : PlayerBaseState, IRootState
     public override void EnterState()
     {
         InitializeSubState();
-        Ctx.Animator.SetBool(Ctx.IsGlidingHash, true);
-        Ctx.CameraManager.SetFPSClampedCamera(true, Ctx.transform.eulerAngles);
+        Ctx.Animator.SetBool("isGliding", true);
     }
 
     public override void UpdateState()
     {
-        HandleGlideMovement();
         HandleGravity();
         CheckSwitchStates();
     }
 
     public override void ExitState()
     {
-        Ctx.Animator.SetBool(Ctx.IsGlidingHash, false);
-        Ctx.CameraManager.SetFPSClampedCamera(false, Ctx.transform.eulerAngles);
-
-        Vector3 rot = Ctx.transform.eulerAngles;
-        rot.x = 0;
-        rot.z = 0;
-        Ctx.transform.eulerAngles = rot;
+        Ctx.Animator.SetBool("isGliding", false);
+        Ctx.IsGlidePressed = false;
     }
 
-    public override void InitializeSubState() {}
+    public override void InitializeSubState()
+    {
+        if (!Ctx.IsMovementPressed && !Ctx.IsRunPressed)
+            SetSubState(Factory.Idle());
+        else if (Ctx.IsMovementPressed && !Ctx.IsRunPressed)
+            SetSubState(Factory.Walk());
+        else
+            SetSubState(Factory.Run());
+    }
 
     public override void CheckSwitchStates()
     {
@@ -43,28 +44,13 @@ public class PlayerGlideState : PlayerBaseState, IRootState
             SwitchState(Factory.Fall());
     }
 
-    private void HandleGlideMovement()
-    {
-        if (Ctx.CurrentMovementInput.magnitude >= 0.1f)
-        {
-            Vector3 rot = Ctx.transform.eulerAngles;
-            rot.x += Ctx.GlideRotationSpeed.x * Ctx.CurrentMovementInput.y * Time.deltaTime;
-            rot.x = Mathf.Clamp(rot.x, Ctx.MinGlideRotationX, Ctx.MaxGlideRotationX);
-            rot.y += Ctx.GlideRotationSpeed.y * Ctx.CurrentMovementInput.x * Time.deltaTime;
-            rot.z += Ctx.GlideRotationSpeed.z * Ctx.CurrentMovementInput.x * Time.deltaTime;
-            Ctx.transform.rotation = Quaternion.Euler(rot);
-        }
-
-        // gerak glide ke depan
-        Vector3 glideVelocity = Ctx.transform.forward * Ctx.GlideSpeed;
-        Ctx.AppliedMovementX = glideVelocity.x;
-        Ctx.AppliedMovementZ = glideVelocity.z;
-    }
-
     public void HandleGravity()
     {
-        float lift = Ctx.transform.rotation.eulerAngles.x;
-        float upForce = (lift + Ctx.AirDrag) * Time.deltaTime;
-        Ctx.AppliedMovementY = Mathf.Max(Ctx.AppliedMovementY + upForce, -20f);
+        float glideGravityMultiplier = 0.2f; 
+        float maxFallSpeed = -3.0f;         
+
+        float previousYVelocity = Ctx.CurrentMovementY;
+        Ctx.CurrentMovementY = Ctx.CurrentMovementY + (Ctx.Gravity * glideGravityMultiplier * Time.deltaTime);
+        Ctx.AppliedMovementY = Mathf.Max((previousYVelocity + Ctx.CurrentMovementY) * .5f, maxFallSpeed);
     }
 }
